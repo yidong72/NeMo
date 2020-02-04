202ed6a3e1c569bc8cf74be0c16588fae8333068
Boris Fomitchev <bfomitchev@nvidia.com>
2020-02-03 22:46:12 -0800

    Fixing build

--- scripts/nano-build.sh
+++ scripts/nano-build.sh
@@ -3,6 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
 cd ${DIR}
 
+export INCLUDE=/usr/local/cuda-10.0/targets/aarch64-linux/include
+export VERBOSE=ON
+
 pip3 install --disable-pip-version-check -U -r requirements.txt && \
     mkdir -p /tmp/onnx-trt && cd /tmp/onnx-trt && rm -fr onnx-tensorrt &&\
     git clone -n https://github.com/onnx/onnx-tensorrt.git && cd onnx-tensorrt && git checkout 8716c9b && git submodule update --init --recursive && patch -f < ${DIR}/scripts/docker/onnx-trt.patch && \
@@ -10,6 +13,7 @@ pip3 install --disable-pip-version-check -U -r requirements.txt && \
     cmake .. -DProtobuf_DEBUG=ON -DProtobuf_USE_STATIC_LIBS=ON \
 	  -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc \
 	  -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/tmp/onnx-trt/install -DGPU_ARCHS="53" && make -j && make install && \
+    mkdir -p /usr/include/aarch64-linux-gnu/ && \
     mv -f /tmp/onnx-trt/install/include/* /usr/include/aarch64-linux-gnu/ \
     && mv -f /tmp/onnx-trt/install/lib/libnvonnx* /usr/lib/aarch64-linux-gnu/ && ldconfig && \
     cd ${DIR}/nemo && pip3 install -e . && cd ../collections/nemo_asr && pip3 install -e .
