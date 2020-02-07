36babe93aeea0278c51b52b0d48b9723280aa502
ASR <bfomitchev@nvidia.com>
2020-02-07 15:36:46 -0800

    fixing packges

--- scripts/nano-export.sh
+++ scripts/nano-export.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+DIR=$(cd $(dirname $0); pwd)
 
 PATH=/usr/src/tensorrt/bin:/usr/local/cuda/bin:${PATH}
 
-cd ${DIR}../
+cd ${DIR}/..
 
 python3 scripts/export_jasper_onnx_to_trt.py \
 	--max-seq-len 768 --seq-len 256 --max-batch-size 1 --batch-size 1 --workspace 512 encoder.onnx encoder.plan decoder.onnx decoder.plan
