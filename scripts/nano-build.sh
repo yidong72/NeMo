0aa872a5cfc3c8629672b06e8720f7bdcfbc11e6
ASR <bfomitchev@nvidia.com>
2020-02-07 19:31:13 -0800

    debug

--- scripts/nano-build.sh
+++ scripts/nano-build.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 cd ${DIR}
 
 export INCLUDE=/usr/local/cuda-10.0/targets/aarch64-linux/include
+# ;/usr/local/lib/python3.6/dist-packages/torch/include
+# export LIB=/usr/local/cuda-10.0/targets/aarch64-linux/lib;/usr/local/lib/python3.6/dist-packages/torch/lib
 export VERBOSE=ON
 export LLVM_CONFIG=/usr/lib/llvm-7/bin/llvm-config
 
